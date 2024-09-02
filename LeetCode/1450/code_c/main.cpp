ce4a3daa850061481fd5bcf3ecbb5cfc481117a0
kimroniny <1301862177@qq.com>
2024-09-02 10:31:12 +0800

    add 1450 cpp

---
+++ LeetCode/1450/code_c/main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int n = startTime.size();
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime) ans++;
+        }
+        return ans;
+    }
+};
